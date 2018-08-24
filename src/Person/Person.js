19fdc364e086c04661b7f61d1945a24cab63bef0
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-08-24 19:01:54 -0400

    3.46 Adding styling with stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react'; // needed for compiling the jsx syntax below
+import './Person.css'; // although we just imported in Person.js, the style will be global as it's injected into <head>
 
 // Use {} to wrap simple javascript calls inside jsx
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             {/* Do not have access to setState because it's not a class-based component */}
             {/* Use the click prop (switchNameHandler) that was passed to Person */}
             <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
