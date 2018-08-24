b3201352aa46a17e2e0ec0a8d4347fe7f755dd5f
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-08-24 17:13:58 -0400

    3.36 Working with Props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react'; // needed for compiling the jsx syntax below
 
 // Use {} to wrap simple javascript calls inside jsx
-const person = () => {
-    return <p>I'm a Person and I'm {Math.ceil(Math.random() * 100)} years old!</p>
+const person = (props) => {
+    return <p>I'm {props.name} and I'm {props.age} years old!</p>
 };
+// If using class-based components, use this.name and this.age to reference the dynamic values
 
 export default person;
