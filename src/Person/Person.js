fa20d2570f4a014a1f8048457b186a7258cc6fa2
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-08-24 17:05:50 -0400

    3.35 Outputting dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react'; // needed for compiling the jsx syntax below
 
+// Use {} to wrap simple javascript calls inside jsx
 const person = () => {
-    return <p>I'm a Person!</p>
+    return <p>I'm a Person and I'm {Math.ceil(Math.random() * 100)} years old!</p>
 };
 
 export default person;
