43161c5a31b0446afaf087e4a7387d8195e653da
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-08-24 01:06:41 -0400

    1.5 Writing Our First React Code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Component (Template)
+function Person(props) {
+    // Use className to avoid conflict with class keyword
+    return (
+        <div className="person">
+            <h1>{props.name}</h1>
+            <p>Your Age: {props.age}</p>
+        </div>
+    );
+}
+
+// Wrapped with () so that we can write multiple lines
+// Pass properties to our Person template
+var app = (
+    <div>
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29" />
+    </div>
+);
+
+ReactDOM.render(app, document.getElementById('root')); // Render app to a single root div (single-page application; recommended)
 registerServiceWorker();
