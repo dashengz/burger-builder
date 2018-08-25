5706dee7f33e74eee2d7c23f08655b11d0f15369
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-08-24 21:36:43 -0400

    4.51 Handling conditional content js way

--- src/App.js
+++ src/App.js
@@ -81,6 +81,27 @@ class App extends Component {
             padding: '5px 10px',
             cursor: 'pointer'
         };
+
+        let persons = null;
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age} />
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, 'Max!') /* bind this, otherwise the runtime this will take over */}
+                        change={this.changeNameHandler}
+                    >My Hobby: Racing</Person>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age} />
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
@@ -89,23 +110,7 @@ class App extends Component {
                     style={buttonStyle}
                     onClick={this.togglePersonsHandler}
                 >Toggle Persons</button>
-                {
-                    this.state.showPersons ?
-                        <div>
-                            <Person
-                                name={this.state.persons[0].name}
-                                age={this.state.persons[0].age} />
-                            <Person
-                                name={this.state.persons[1].name}
-                                age={this.state.persons[1].age}
-                                click={this.switchNameHandler.bind(this, 'Max!') /* bind this, otherwise the runtime this will take over */}
-                                change={this.changeNameHandler}
-                            >My Hobby: Racing</Person>
-                            <Person
-                                name={this.state.persons[2].name}
-                                age={this.state.persons[2].age} />
-                        </div> : null
-                }
+                {persons}
             </div>
         );
     }
