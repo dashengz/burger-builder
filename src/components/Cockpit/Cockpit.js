c6318e696bf12f7ccd979d15ae4228cb0a68a8b6
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-08-26 19:17:21 -0400

    7.81 Comparing stateless and stateful components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -11,7 +11,7 @@ const cockpit = (props) => {
     if (props.persons.length < 2) classNames.push(classes.bold);
     return (
         <div className={classes.Cockpit}>
-            <h1>Hi, I'm a React App</h1>
+            <h1>{props.appTitle}</h1>
             <p className={classNames.join(' ')}>This is really working!</p>
             <button
                 className={buttonClass}
--- src/containers/App.js
+++ src/containers/App.js
@@ -71,6 +71,7 @@ class App extends Component {
         return (
             <div className={classes.App}>
                 <Cockpit
+                    appTitle={this.props.title}
                     persons={this.state.persons}
                     showPersons={this.state.showPersons}
                     click={this.togglePersonsHandler}
