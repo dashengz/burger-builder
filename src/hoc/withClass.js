a297ebae0a73e65ab5b9d430b5d96a1d08c3e9e3
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-08-26 22:04:01 -0400

    7.97 Passing unknown props / Stateful HOC

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
+// const withClass = (WrappedComponent, classes) => {
+//     return (props) => {
+//         return (
+//             <div className={classes}>
+//                 <WrappedComponent {...props} /> {/* Spread the props into key value pairs */}
+//             </div>
+//         );
+//     };
+// };
+
+// HOC can also be stateful components
 const withClass = (WrappedComponent, classes) => {
-    return (props) => {
-        return (
-            <div className={classes}>
-                <WrappedComponent />
-            </div>
-        );
+    return class extends Component {
+        componentDidMount() {
+            console.log('HOC - withClass - componentDidMount');
+        }
+        render() {
+            return (
+                <div className={classes}>
+                    <WrappedComponent {...this.props} />
+                </div>
+            );
+        }
     };
 };
 
