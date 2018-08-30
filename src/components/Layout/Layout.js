ffffd6c0b345af7d8519b572847d4437f7f0cfe4
Jonathan Dasheng Zhang <dz2276@tc.columbia.edu>
2018-08-30 17:08:20 -0400

    8.137 Reusing the backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Layout.css';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-    <React.Fragment>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </React.Fragment>
-);
+class Layout extends Component {
+    // We want to control the state of the menu in both Toolbar and SideDrawer
+    // We can do that by using the state, thus we need to change Layout to class-based component
 
-export default layout;
+    state = {
+        showSideDrawer: true
+    };
+
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        });
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <Toolbar />
+                <SideDrawer show={this.state.showSideDrawer} close={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,21 @@ import React from 'react';
 import classes from './SideDrawer.css';
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const sideDrawer = (props) => {
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}><Logo /></div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <React.Fragment>
+            <Backdrop show={props.show} click={props.close} />
+            <div className={
+                [classes.SideDrawer, props.show ? classes.Open : classes.Close].join(' ')
+            }>
+                <div className={classes.Logo}><Logo /></div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </React.Fragment>
     );
 };
 
