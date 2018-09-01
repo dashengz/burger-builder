5ce9d760f035274fb1930fcde0301b697241726d
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-08-31 22:31:53 -0400

    8.139 Adding a hamburger icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -4,7 +4,9 @@ import classes from './DrawerToggle.css';
 const drawerToggle = (props) => {
     return (
         <div className={classes.DrawerToggle} onClick={props.click}>
-            MENU
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     );
 };
