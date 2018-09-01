2bba0499ba94c9e5aa650f540afe1490d2195687
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-08-31 23:06:21 -0400

    8.142 Improving performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(key => {
-            return (
-                <li key={key}>
-                    <span style={{textTransform: 'capitalize'}}>{key}</span>: {props.ingredients[key]}
-                </li>
-            );
-        });
-    return (
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button type="Danger" click={props.cancel}>CANCEL</Button>
-            <Button type="Success" click={props.checkout}>CONTINUE</Button>
-        </React.Fragment>
-    );
-};
+class OrderSummary extends Component {
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("OrderSummary - getSnapshotBeforeUpdate");
+        return null;
+    }
 
-export default orderSummary;
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("OrderSummary - componentDidUpdate");
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(key => {
+                return (
+                    <li key={key}>
+                        <span style={{textTransform: 'capitalize'}}>{key}</span>: {this.props.ingredients[key]}
+                    </li>
+                );
+            });
+        return (
+            <React.Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button type="Danger" click={this.props.cancel}>CANCEL</Button>
+                <Button type="Success" click={this.props.checkout}>CONTINUE</Button>
+            </React.Fragment>
+        );
+    }
+}
+
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => {
-    return (
-        <React.Fragment>
-            <Backdrop show={props.show} click={props.dismiss} />
-            <div
-                className={classes.Modal}
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
-                {props.children}
-            </div>
-        </React.Fragment>
-    );
-};
+class Modal extends Component {
+    // Since we are only checking props.show, not props.click,
+    // we don't have to use PureComponent
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("Modal - getSnapshotBeforeUpdate");
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("Modal - componentDidUpdate");
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Backdrop show={this.props.show} click={this.props.dismiss} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Modal;
