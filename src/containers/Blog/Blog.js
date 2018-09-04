915aa506b8c272c74a750f0b803c1faf07406956
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-09-04 00:39:33 -0400

    11.203 Loading routes lazily

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
-import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+
+import asyncComponent from "../../hoc/asyncComponent/asyncComponent";
+// import NewPost from "./NewPost/NewPost";
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     };
 
     render() {
@@ -44,7 +49,7 @@ class Blog extends Component {
                         // If not authenticated then don't output new post
                         // Routing is essentially as rendering components
                         this.state.auth ?
-                            <Route path="/new-post" component={NewPost}/> :
+                            <Route path="/new-post" component={AsyncNewPost}/> :
                             null
                     }
                     <Route path="/posts" component={Posts}/>
