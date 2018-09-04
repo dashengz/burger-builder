43b46ad68652f3a5badf6c9fd50831d40711394e
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-09-04 00:45:04 -0400

    11.204 Routing and server deployment

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
     render() {
         return (
+            // If you serve the app to a sub-domain
+            // <BrowserRouter basename="/my-app">...</BrowserRouter>
+            // Also, the server needs to be configured to handle 404,
+            // redirects to index.html if the page is unknown!
+
             <BrowserRouter>
                 <div className="App">
                     <Blog/>
