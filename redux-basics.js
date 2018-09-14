f46ce1efec83fa9ac1ae6853e086c21fed5315c0
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-09-13 23:02:15 -0400

    14.243 Dispatching actions

--- redux-basics.js
+++ redux-basics.js
@@ -15,6 +15,18 @@ const initialState = {
 // multiple reducers will be merged into one
 // should not have side-effects, must be synchronous
 const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }; // must update state immutably
+        case 'ADD_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }; // must update state immutably
+    }
     return state;
 };
 
@@ -24,5 +36,13 @@ const store = createStore(rootReducer);
 console.log(store.getState()); // will be printed to console if we run 'node redux-basics.js'
 
 // Dispatching Action
+store.dispatch({
+    type: 'INC_COUNTER' // 'type' must be specified; ALL CAPS is the convention
+});
+store.dispatch({
+    type: 'ADD_COUNTER',
+    value: 10 // Additional payload; can be anything you want: js objects, string, number, ...
+});
+console.log(store.getState());
 
 // Subscription
