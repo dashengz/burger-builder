2f8a29928ef145fe2235344a280d34d074a71815
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-09-16 22:11:57 -0400

    16.280 A leaner switch case statement

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,24 +5,29 @@ const initialState = {
     results: []
 };
 
+const storeResult = (state, action) => {
+    return state.results.concat({
+        id: new Date().getTime(),
+        // value: state.counter // not accessible anymore
+        value: action.result // need to switch to using action payload!
+    });
+};
+
+const deleteResult = (state, action) => {
+    return state.results.filter(r =>
+        r.id !== action.resultElementId
+    )
+};
+
 // reducers cannot handle async code!
 // need to use action creators
 const reducer = (state = initialState, action) => {
+    // Create a leaner switch statement helps you see clearly what cases you handle
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return updateObject(state, {
-                results: state.results.concat({
-                    id: new Date().getTime(),
-                    // value: state.counter // not accessible anymore
-                    value: action.result // need to switch to using action payload!
-                })
-            });
+            return updateObject(state, {results: storeResult(state, action)});
         case actionTypes.DELETE_RESULT:
-            return updateObject(state, {
-                results: state.results.filter(r =>
-                    r.id !== action.resultElementId
-                )
-            });
+            return updateObject(state, {results: deleteResult(state, action)});
         default:
     }
     return state;
