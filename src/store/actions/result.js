355b8c75a100e2b55c897013d7103db36e27d24d
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-09-16 21:51:48 -0400

    16.278 Using action creators and get state

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -10,9 +10,15 @@ const save = result => {
 // utility step to run async code
 // thunk will block this async action, and dispatch the actual sync action
 export const storeResult = (result) => {
-    return dispatch => {
+    return (dispatch, getState) => {
         // with the help of react-thunk, we can dispatch asynchronously
         setTimeout(() => {
+            // We could access the current state here, and do something with it
+            // eg. console.log(getState().ctr.counter);
+
+            // But we shouldn't, because we could also just
+            // get the state we want by passing it down from the container,
+            // and accepting it as an argument here
             dispatch(save(result));
         }, 2000);
     };
