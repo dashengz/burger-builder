fc009f9de20f47bea00e5f0ea4574a079cd8a57c
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-09-17 01:28:41 -0400

    17.299 Handling purchases and updating the UI

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,8 +3,15 @@ import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSumm
 import {Redirect, Route} from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
 import {connect} from "react-redux";
+import * as actionCreators from '../../store/actions/index';
 
 class Checkout extends Component {
+    constructor(props) {
+        super(props);
+
+        this.props.onInitPurchase();
+        // Too late to modify purchased prop, because the render received the old prop (purchased true)
+    }
 
     cancelHandler = () => {
         this.props.history.goBack();
@@ -15,31 +22,33 @@ class Checkout extends Component {
     };
 
     render() {
-        return (
-            <div>
-                {
-                    this.props.ingredients ?
-                        (
-                            <React.Fragment>
-                                <CheckoutSummary
-                                    ingredients={this.props.ingredients}
-                                    cancel={this.cancelHandler}
-                                    checkout={this.checkoutHandler}/>
-                                <Route
-                                    path={this.props.match.path + '/contact-data'}
-                                    component={ContactData}/>
-                            </React.Fragment>
-                        ) : <Redirect to="/"/>
-                }
-            </div>
-        );
+        return this.props.ingredients ?
+            (
+                <div>
+                    {this.props.purchased ? <Redirect to="/"/> : null}
+                    <CheckoutSummary
+                        ingredients={this.props.ingredients}
+                        cancel={this.cancelHandler}
+                        checkout={this.checkoutHandler}/>
+                    <Route
+                        path={this.props.match.path + '/contact-data'}
+                        component={ContactData}/>
+                </div>
+            ) : <Redirect to="/"/>
     }
 }
 
 const mapStateToProps = state => {
     return {
-        ingredients: state.burgerBuilder.ingredients
+        ingredients: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     };
 };
 
-export default connect(mapStateToProps)(Checkout);
+const mapDispatchToProps = dispatch => {
+    return {
+        onInitPurchase: () => dispatch(actionCreators.purchaseInit())
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
