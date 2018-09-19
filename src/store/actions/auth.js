0c0977f246f286d4d334643d74aa033c3d998a05
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-09-18 22:01:03 -0400

    18.314 Storing the token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     }
 };
 
-export const authSuccess = authData => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        token: token,
+        userId: userId
     }
 };
 
@@ -47,7 +48,10 @@ export const auth = (email, password, method = AUTH_SIGN_UP) => {
             }
         ).then(response => {
             console.log(response);
-            dispatch(authSuccess(response.data));
+            // idToken -> token, localId -> userId
+            dispatch(authSuccess(
+                response.data.idToken,
+                response.data.localId));
         }).catch(err => {
             console.log(err);
             dispatch(authFailed(err));
