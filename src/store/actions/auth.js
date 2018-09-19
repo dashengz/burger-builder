4698cbe32a500d2b0e05c6c31a24ba4876afa06d
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-09-18 20:52:00 -0400

    18.312 Getting a token from the backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from "axios";
 
 export const authStart = () => {
     return {
@@ -25,5 +26,20 @@ export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
         // ... auth user
+        axios.post(
+            // see https://firebase.google.com/docs/reference/rest/auth/ for more endpoints
+            'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBvoYKbktcDCcZjHcE4dFUoghshr9sNU8Y',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).then(response => {
+            console.log(response);
+            dispatch(authSuccess(response.data));
+        }).catch(err => {
+            console.log(err);
+            dispatch(authFailed(err));
+        });
     };
 };
