da1ab7efd185e8e63bd00dfca5fa35cc28f3a859
Dasheng Jonathan Zhang <contact@dashengz.com>
2018-09-20 20:17:36 -0400

    20.344 Jest and Enzyme documentations

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,3 +1,6 @@
+// jest doc: https://jestjs.io/docs/en/getting-started.html
+// enzyme doc: https://airbnb.io/enzyme/
+
 import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import NavigationItems from "./NavigationItems";
@@ -27,5 +30,15 @@ describe('<NavigationItems />', () => {
             isAuthed: true
         });
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render logout <NavigationItem /> if authenticated', () => {
+        // beforeEach() set the value for wrapper, thus isAuthed was not set
+        // isolated from other it()s, so we need to set the isAuthed again
+        wrapper.setProps({
+            isAuthed: true
+        });
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>))
+            .toEqual(true);
     });
 });
